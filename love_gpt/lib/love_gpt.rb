e4440be5411111ef3357a36c28081e033296d2d8
Andrii Ukrainets <aukra@softserveinc.com>
2023-07-05 22:53:34 +0200

    add draft of love_GPT gem

---
+++ love_gpt/lib/love_gpt.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "love_gpt/version"
+
+module LoveGpt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ love_gpt/test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "love_gpt"
+
+require "minitest/autorun"
